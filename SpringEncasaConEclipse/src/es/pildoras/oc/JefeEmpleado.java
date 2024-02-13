149913b74c33b1814973920c4390844c3ad32a08
Silvia Mesa Cofrades <120189826+silviamescof@users.noreply.github.com>
2024-02-13 19:00:57 +0100

    19:00

---
+++ SpringEncasaConEclipse/src/es/pildoras/oc/JefeEmpleado.java
@@ -0,0 +1,7 @@
+package es.pildoras.oc;
+
+public class JefeEmpleado {
+	public String getTareas() {
+		return "Gestiono las cuestiones de mis empleafos";
+	}
+}
---
+++ SpringEncasaConEclipse/src/es/pildoras/oc/UsoEmpleados.java
@@ -0,0 +1,18 @@
+package es.pildoras.oc;
+
+public class UsoEmpleados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//creacion de objetos de tipo empleado
+			JefeEmpleado Empleado1=new JefeEmpleado();
+			
+		
+		//uso de los objetos creados
+			
+			System.out.println(Empleado1.getTareas());
+	}
+
+}
